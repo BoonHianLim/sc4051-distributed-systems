a1ebc7e13a49a461dd562670cc39e2dc766e4a18
tayweihong <tayweihong123@gmail.com>
2025-03-14 17:23:42 +0800

    implemented atmostonce in socket.py and socket.java

--- client/src/comm/socket.py
+++ client/src/comm/socket.py
@@ -7,7 +7,6 @@ from src.comm.parser import Parser
 
 logger = logging.getLogger(__name__)
 
-
 class Socket():
     def __init__(self):
         pass
@@ -17,7 +16,6 @@ class Socket():
 
     def listen(self):
         pass
-
 
 class AtLeastOnceSocket(Socket):
     buffer: list[dict] = []
@@ -99,6 +97,102 @@ class AtLeastOnceSocket(Socket):
         self.listen_thread = None
         self.shutdown_flag = False
 
-
+# work on this (wei)
 class AtMostOnceSocket(Socket):
-    pass
+    # High level requirements
+    # 1. Client re-transmits requests
+        # Occurs when client does not receive a response in time, basically timeout
+    # 2. Server filter duplicates
+    # 3. Client sends ACK
+        # After client gets valid response, it sends an ACK to the server
+        # Server stops re-sending response once it receives ACK or max attempts
+
+    buffer: list[dict] = []
+    shutdown_flag = False
+    listen_thread = None
+
+    def __init__(self, parser: Parser, timeout_seconds: int = 60,
+                 ip_addr: str = "127.0.0.1", port: int = 11999):
+        super().__init__()
+        self.parser = parser
+        self.socket = socket(AF_INET, SOCK_DGRAM)
+        self.timeout_seconds = timeout_seconds
+        self.addr = (ip_addr, port)
+
+    def send(self, message: any, service_id: int, is_request: bool,
+             server_addr: str = "127.0.0.1", port: int = 12000) -> any:
+        request_id = uuid4()
+        msg_in_bytes = self.parser.marshall(request_id, service_id, is_request, message)
+        addr = (server_addr, port)
+
+        while True:
+            # (a) send request
+            self.socket.sendto(msg_in_bytes, addr)
+            logger.debug(f"[AtMostOnceSocket] Sent request {request_id}")
+            print(f"[AtMostOnceSocket] Sent request {request_id}")
+
+            # (b) wait time out secs for response to appear in buffer
+            start_time = time.time()
+            while True:
+                # break if no response after timeout
+                if time.time() - start_time >= self.timeout_seconds:
+                    break
+                if self.shutdown_flag:
+                    raise ConnectionError("Socket is closed")     
+                # check if we have anything in buffer
+                if len(self.buffer) > 0:
+                    obj = self.buffer.pop()
+                    if obj["request_id"] == request_id:
+                        logger.debug(f"[AtMostOnceSocket] Received matching response for {request_id}")
+                        print(f"[AtMostOnceSocket] Received matching response for {request_id}")
+
+                        # (c) Send ACK to server
+                        ack_bytes = self.parser.marshall_ack(request_id)
+                        self.socket.sendto(ack_bytes, addr)
+                        logger.debug(f"sent ACK for {request_id}")
+                        print(f"sent ACK for {request_id}")
+                        # Return the object
+                        return obj
+                    else:
+                        # Not the correct response => ignore
+                        logger.debug(f"[AtMostOnceSocket] Ignoring unrelated response: {obj['request_id']}")
+                        print(f"[AtMostOnceSocket] Ignoring unrelated response: {obj['request_id']}")
+                time.sleep(0.1)
+
+            # If we reach here, no matching response arrived => re-send
+            logger.debug("[AtMostOnceSocket] No matching response, re-sending request...")
+            print("[AtMostOnceSocket] Timeout, re-sending request...")
+
+    def listen(self):
+        # to bind socket to self.addr, while continuously receving packets
+        if self.listen_thread is not None:
+            return  
+        self.socket.bind(self.addr)
+        self.shutdown_flag = False
+        self.listen_thread = threading.Thread(target=self._listen, daemon=True)
+        self.listen_thread.start()
+
+    def _listen(self):
+        while not self.shutdown_flag:
+            try:
+                recv_bytes, _ = self.socket.recvfrom(1024)
+                obj = self.parser.unmarshall(recv_bytes)
+                self.buffer.append(obj)
+            except timeout:
+                logger.error("[AtMostOnceSocket] Socket timeout in listen thread")
+            except OSError as os_err:
+                if self.shutdown_flag:
+                    logger.debug("[AtMostOnceSocket] Socket closed due to shutdown.")
+                else:
+                    logger.error("[AtMostOnceSocket] Socket error: %s", os_err)
+                break
+    
+    def close(self):
+        # shut socket down
+        if self.listen_thread is not None:
+            self.shutdown_flag = True
+            self.socket.close()
+            self.listen_thread.join()
+
+        self.listen_thread = None
+        self.shutdown_flag = False
